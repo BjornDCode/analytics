0e8636e85753ba02d5c87feeee32e83ef4eb022e
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-12-18 02:05:48 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     purge: {
         layers: [],
+        content: [],
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
